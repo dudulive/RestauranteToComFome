f471cbe3cebff46a6fd5b247c4b4e6a5a7986da7
dudulive <dudurodriguesbraz@gmail.com>
2016-07-14 21:55:49 -0300

    Finalização do crud usuario permissão.

--- grails-app/controllers/restaurantetocomfome/ControlePermissaoController.groovy
+++ grails-app/controllers/restaurantetocomfome/ControlePermissaoController.groovy
@@ -1,81 +1,138 @@
 package restaurantetocomfome
 
-import com.sun.beans.decoder.FalseElementHandler;
+import grails.converters.JSON
 
-import grails.converters.JSON;
-import comun.Permissao;
-import comun.Usuario;
+import comun.Permissao
+import comun.Usuario
+import comun.UsuarioPermissao
+
 
 class ControlePermissaoController {
 
-    def index() { 
-		def listaPermissoes = Permissao.list()
-		render(view:"index", model: [permissoes:listaPermissoes])
-	}
-	
-	def listarUsuarios(){
-		def listaUsuarios = Usuario.createCriteria().list{
-			order("username")
-		}
-		render(template:"listaUsuarios", model: [usuarios:listaUsuarios] ) 
-	}
-	
-	def getPermissao(){
-		Permissao permissao = Permissao.get(params.id);
-		render permissao as JSON
+	def index() {
+		render(view:"index")
 	}
 	
 	def getUsuario(){
-		
+		Usuario usuario = Usuario.get(params.id)
+		def retorno = [:]
+		retorno["usuario"] = usuario
+		retorno["permissoes"] = usuario.getAuthorities()
+		render retorno as JSON
+	}
+	
+	def getPermissao(){
+		Permissao permissao = Permissao.get(params.id)
+		render permissao as JSON
 	}
 	
 	def listarPermissao(){
 		def listaPermissoes = Permissao.list()
-		render(template:"listaPermissao", model: [permissoes:listaPermissoes])
+		render(template: "listaPermissao", model: [permissoes: listaPermissoes])
 	}
 	
 	def salvarPermissao(){
 		def retorno = [:]
-		Permissao permissao ;
-		if(params.id){
-			permissao = Permissao.get(params.id)	
+		Permissao permissao
+		if (params.id){
+			permissao = Permissao.get(params.id)
 		}else{
-		 permissao = new Permissao();
+			permissao=new Permissao()
 		}
 		permissao.authority = params.permissao
 		permissao.validate()
-		if(!permissao.hasErrors()){
-		 permissao.save(flush:true)
+		if (!permissao.hasErrors()){
+			permissao.save(flush:true)
 			retorno["mensagem"]="OK"
 		}else{
-			retorno["mensagem"]="ERROR"
+			retorno["mensagem"]="ERRO"
 		}
 		render retorno as JSON
 	}
 	
 	def excluirUsuario(){
-		
+		def retorno=[:]
+		Usuario usuario=Usuario.get(params.id)
+		try{
+			usuario.delete(flush:true)
+			retorno["mensagem"]="OK"
+		}catch(Exception ex){
+			retorno["mensagem"]="ERRO"
+		}
+		render retorno as JSON
 	}
 	
-	def excuirPermissao(){
-		
+	def excluirPermissao(){
+		def retorno=[:]
+		Permissao permissao=Permissao.get(params.id)
+		try{
+			permissao.delete(flush:true)
+			retorno["mensagem"]="OK"
+		}catch(Exception ex){
+			retorno["mensagem"]="ERRO"
+		}
+		render retorno as JSON
+	}
+	
+	def listarUsuario(){
+		def listaUsuarios = Usuario.createCriteria().list{
+			order("username")
+		}
+		render(template: "listaUsuarios", model:[usuarios: listaUsuarios])
 	}
 	
 	def salvarUsuario(){
 		def retorno = [:]
-		Usuario usuario = new Usuario();
+		
+		Usuario usuario
+		if (params.id){
+			usuario = Usuario.get(params.id)
+		}else{
+			usuario = new Usuario()
+			usuario.enabled = true
+			usuario.passwordExpired = false
+			usuario.accountExpired = false
+			usuario.accountLocked = false
+			usuario.password = "1234"
+		}
 		usuario.username = params.login
-		usuario.enabled = true
-		usuario.passwordExpired = false
-		usuario.accountExpired = false
-		usuario.accountLocked = false
-		usuario.password = "1234"
 		usuario.validate()
-		if(!usuario.hasErrors()){
-		 usuario.save(flush:true)
+		if (!usuario.hasErrors()){
+			usuario.save(flush:true)
 			retorno["mensagem"]="OK"
 		}else{
-			retorno["mensagem"]="ERROR"
+			retorno["mensagem"]="ERRO"
+		}
+		render retorno as JSON
+	}
+	
+	def vincularPermissao(){
+		def retorno = [:]
+		Usuario usuario = Usuario.get(params.idUsuario)
+		Permissao permissao = Permissao.get(params.idPermissao)
+		
+		try{
+			UsuarioPermissao.create(usuario,permissao,true)
+			retorno["mensagem"]="OK"
+		}catch(Exception ex){
+			retorno["mensagem"]="ERRO"
+		}
+		render retorno as JSON
+	}
+	
+	def desvincularPermissao(){
+		def retorno = [:]
+		Usuario usuario = Usuario.get(params.idUsuario)
+		Permissao permissao = Permissao.get(params.idPermissao)
+		
+		try{
+			if (UsuarioPermissao.remove(usuario,permissao,true)){
+				retorno["mensagem"]="OK"
+			}else{
+				retorno["mensagem"]="ERRO"
+			}
+		}catch(Exception ex){
+			retorno["mensagem"]="ERRO"
 		}
 		render retorno as JSON
 	}
--- grails-app/views/controlePermissao/_listaUsuarios.gsp
+++ grails-app/views/controlePermissao/_listaUsuarios.gsp
@@ -1,12 +1,12 @@
 <table>
-	<thead>
-		<th>Nome do Usuario</th>
-	</thead>
-	<g:each in="${usuarios}" var="usuario">
-		<tr>
-			<td>
-				${usuario.username}
-			</td>
-		</tr>
-	</g:each>
+<thead>
+	<th colspan="3">Nome do usuário</th>
+</thead>
+<g:each in="${usuarios}" var="usuario">
+	<tr>
+		<td>${usuario.username}</td>
+		<td><a href="javascript: alterarUsuario(${usuario.id})">Alterar</a></td>
+		<td><a href="javascript: excluirUsuario(${usuario.id})">Excluir</a></td>
+	</tr>
+</g:each>
 </table>
