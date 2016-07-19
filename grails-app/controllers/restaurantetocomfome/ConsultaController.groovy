430c1430dda08930da4a986fc25383c78a07fe5f
dudulive <dudurodriguesbraz@gmail.com>
2016-07-18 22:30:34 -0300

    Testes com consultas.

--- grails-app/controllers/restaurantetocomfome/ConsultaController.groovy
+++ grails-app/controllers/restaurantetocomfome/ConsultaController.groovy
@@ -5,14 +5,14 @@ class ConsultaController {
     def index() {
 		def lista = []
 		
-		//lista = Cliente.findAll();
+		lista = Bebida.findAllByLiquidoGreaterThanAndUnidade(500,"ml");
 		
-		lista = Cliente.createCriteria().list {
+		/*lista = Cliente.createCriteria().list {
 			order("nome", "desc")
-		}
+		}*/
 
 		//lista = Cliente.executeQuery("select cli from Cliente cli order by nome desc")
 				
-		render(view:"index", model: [lista: lista.nome])
+		render(view:"index", model: [lista: lista.liquido])
 	}
 }
